15776304f2658f2d30e29517a9371f3b0eef69da
hierarch <riichinomics@gmail.com>
2020-06-10 00:01:47 +1200

    Changing indents

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,42 +1,42 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
-  if (!env) {
-    env = {}
-  }
-  return {
-    mode: env.production ? 'production' : 'development',
-    devtool: "source-map",
-    resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "awesome-typescript-loader"
-            }
-          ]
-        },
-        {
-          enforce: "pre",
-          test: /\.js$/,
-          loader: "source-map-loader"
-        }
-      ]
-    },
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: "Majsoul API Frontend"
-      })
-    ]
-  };
+	if (!env) {
+		env = {}
+	}
+	return {
+		mode: env.production ? 'production' : 'development',
+		devtool: "source-map",
+		resolve: {
+			extensions: [".ts", ".tsx", ".js", ".json"]
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts(x?)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: "awesome-typescript-loader"
+						}
+					]
+				},
+				{
+					enforce: "pre",
+					test: /\.js$/,
+					loader: "source-map-loader"
+				}
+			]
+		},
+		optimization: {
+			splitChunks: {
+				chunks: 'all'
+			}
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: "Majsoul API Frontend"
+			})
+		]
+	};
 };
