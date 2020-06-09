d1fade955e2d8f45aa74e27e445445e3e513dac2
hierarch <riichinomics@gmail.com>
2020-06-10 01:24:32 +1200

    Added react-router, redux store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+import thunkMiddleware from 'redux-thunk';
 import { Hello } from "./components/Hello";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
 	(state) => state,
-	(window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+	composeEnhancers(
+		applyMiddleware(
+			thunkMiddleware
+		)
+	)
 );
 
 ReactDOM.render(
 	<Provider store={store}>
-	<Hello compiler="TypeScript" framework="React" />
+		<BrowserRouter>
+			<Route exact path="/">
+				<Hello compiler="TypeScript" framework="React" />
+			</Route>
+		</BrowserRouter>
 	</Provider>,
-		document.getElementsByTagName("body")[0]
+	document.getElementsByTagName("body")[0]
 );
