f389c532bf6ed94254cf31ea04f3cb611a1d4c05
hierarch <riichinomics@gmail.com>
2020-06-10 00:00:05 +1200

    adding redux store and devtools

--- frontend/src/components/Hello.tsx
+++ frontend/src/components/Hello.tsx
@@ -5,7 +5,7 @@ export interface HelloProps { compiler: string; framework: string; }
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
+	render() {
+		return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+	}
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(
+	(state) => state,
+	(window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementsByTagName("body")[1]
+	<Provider store={store}>
+	<Hello compiler="TypeScript" framework="React" />
+	</Provider>,
+		document.getElementsByTagName("body")[0]
 );
-alert("test");
