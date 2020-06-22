45c0fd47418aa89baf559ea7092c8cec0ec9e8db
hierarch <riichinomics@gmail.com>
2020-06-23 03:40:31 +1200

    Added text time edit with moment validation

--- frontend/src/components/Session.tsx
+++ frontend/src/components/Session.tsx
@@ -6,25 +6,54 @@ import Col from 'react-bootstrap/Col';
 import * as moment from "moment-timezone";
 import { CountdownTimer } from "./CountdownTimer";
 import { Match } from "./Match";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { GameResultSummary } from "./GameResultSummary";
 import { fetchGamesHook } from "../Actions";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export function Session(props: {
 	session: Session;
 }): JSX.Element{
-	const teams = useSelector((state: IState) => state.contest.teams);
+	const teams = useSelector((state: IState) => state.contest.teams, shallowEqual);
+	const token = useSelector((state: IState) => state.user?.token);
+
 	const dispatch = useDispatch();
 	React.useEffect(() => {
 		fetchGamesHook(dispatch, {sessionIds: [props.session._id]});
 	}, [props.session._id]);
+
+	const utcStartMoment = moment(props.session.scheduledTime).tz("UTC");
+
+	const [utcMoment, setUtcMoment] = React.useState(utcStartMoment.format("LT l") + " UTC");
+	const [timeIsInvalid, setTimeIsValid] = React.useState(!utcStartMoment.isValid());
+	const [editTime, setEditTime] = React.useState(false);
 
 	return <Container fluid className="bg-dark rounded text-light">
 		<Row className="py-3 px-2">
 			<Col md="auto">
 				<Container>
 					<Row>
-						{moment(props.session.scheduledTime).tz("UTC").format("LT l")} UTC
+						<Form.Control
+							plaintext={!token || !editTime}
+							readOnly={!token || !editTime}
+							isInvalid={timeIsInvalid}
+							className={`py-0${(!token || !editTime) ? " text-light" : ""}`}
+							value={`${utcMoment}`}
+							onChange={event => {
+								setUtcMoment(event.target.value)
+								setTimeIsValid(!moment(event.target.value).isValid())
+							}}
+							onFocus={(event: any) => setEditTime(true)}
+							onBlur={(event: any) => {
+								if (timeIsInvalid) {
+									setUtcMoment(moment(props.session.scheduledTime).tz("UTC").format("LT l") + " UTC")
+								} else {
+									setUtcMoment(moment(event.target.value).tz("UTC").format("LT l") + " UTC")
+								}
+								setEditTime(false);
+							}}
+						/>
 					</Row>
 					<Row>
 						{moment(props.session.scheduledTime).calendar()} in {moment.tz.guess()}
@@ -45,5 +74,10 @@ export function Session(props: {
 				<GameResultSummary game={game}/>
 			</Container>)}
 		</Row>
+		{ (token && !timeIsInvalid && !utcStartMoment.isSame(moment(utcMoment))) &&
+			<Row className="pb-3 px-3 justify-content-end">
+				<Button variant="secondary" onClick={(event: any) => {}} >Save</Button>
+			</Row>
+		}
 	</Container>;
 }
