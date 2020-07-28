f57583ed298bd73489ec7c7eb0e230c99c35c391
hierarch <riichinomics@gmail.com>
2020-07-29 11:15:36 +1200

    Split contest inforation from contest frame

--- frontend/src/components/ContestSummary.tsx
+++ frontend/src/components/ContestSummary.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { LeagueStandingChart } from "./LeagueStandingChart";
 import { fetchContestSummary, fetchContestSessions, ActionType } from "../Actions";
-import { IState } from "../State";
+import { IState, Contest } from "../State";
 import { useSelector, useDispatch } from "react-redux";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -9,8 +9,6 @@ import Col from 'react-bootstrap/Col';
 import { Session } from "./Session";
 import { Teams } from "./Teams";
 import YouTube from 'react-youtube';
-import ronImage from '../../assets/ron.png';
-import skipImage from '../../assets/skip.png';
 
 function SongPlayer(props: {videoId: string, play?: boolean}): JSX.Element {
 	const [player, setPlayer] = React.useState<YT.Player>(null);
@@ -31,11 +29,15 @@ function SongPlayer(props: {videoId: string, play?: boolean}): JSX.Element {
 	</div>
 }
 
-export function ContestSummary(this: void, props: {contestId: string}): JSX.Element {
+export function ContestSummary(props: {contestId: string}): JSX.Element {
 	const contest = useSelector((state: IState) => state.contest);
-	const games = useSelector((state: IState) => Object.values(state.games ?? [])?.sort((a, b) => b.end_time - a.end_time));
-	const musicPlayer = useSelector((state: IState) => state.musicPlayer);
 	const dispatch = useDispatch();
+
+	React.useEffect(() => {
+		fetchContestSummary(dispatch, props.contestId);
+		fetchContestSessions(dispatch, props.contestId);
+	}, [props.contestId]);
+
 	const [secret, setSecret] = React.useState(false);
 	React.useEffect(() => {
 		if (!secret) {
@@ -45,10 +47,40 @@ export function ContestSummary(this: void, props: {contestId: string}): JSX.Elem
 		setTimeout(() => setSecret(false), 5000);
 	}, [secret]);
 
-	React.useEffect(() => {
-		fetchContestSummary(dispatch, props.contestId);
-		fetchContestSessions(dispatch, props.contestId);
-	}, [props.contestId]);
+	return <Container>
+		<Row className="px-4 pt-4 pb-3 no-gutters align-items-center">
+			<Col md="auto">
+				<h1 onClick={() => setSecret(true)}><u style={{cursor: "pointer"}}>{contest?.name}</u></h1>
+			</Col>
+			<Col md="auto">
+				<i>
+					{secret
+						? "They said I could be anything, so I became yakitori."
+						: "We're going down, and sugar we're going down swinging."}
+					</i>
+				</Col>
+		</Row>
+
+		{contest != null ? <LeagueContestSummary contest={contest}/> : null}
+
+		<Row className="mt-3 justify-content-center">
+			<Col md="auto"><a className="text-dark" href="https://boards.4channel.org/vg/catalog#s=mjg">/mjg/</a></Col>
+			<Col md="auto"><a className="text-dark" href="https://repo.riichi.moe/">Repo</a></Col>
+			<Col md="auto"><a className="text-dark" href="https://github.com/riichinomics/majsoul-api">Source Code/Report Issue</a></Col>
+		</Row>
+	</Container>
+}
+
+export function TournamentContestSummary(): JSX.Element {
+	return null;
+}
+
+export function LeagueContestSummary(props: {contest: Contest}): JSX.Element {
+	const games = useSelector((state: IState) => Object.values(state.games ?? [])?.sort((a, b) => b.end_time - a.end_time));
+	const musicPlayer = useSelector((state: IState) => state.musicPlayer);
+	const dispatch = useDispatch();
+
+	const contest = props.contest;
 
 	const nextSessionIndex = contest?.sessions?.findIndex(session => session.scheduledTime > Date.now()) ?? -1;
 	const nextSession = contest?.sessions == null ? null : contest.sessions[nextSessionIndex];
@@ -79,57 +111,26 @@ export function ContestSummary(this: void, props: {contestId: string}): JSX.Elem
 		return null;
 	}
 
-	return <Container>
-		<SongPlayer videoId={musicPlayer.videoId} play={musicPlayer.playing}/>
-		<Row className="px-4 pt-4 pb-3 no-gutters align-items-center">
-			<Col md="auto">
-				<h1 onClick={() => setSecret(true)}><u style={{cursor: "pointer"}}>{contest.name}</u></h1>
-			</Col>
-			<Col>
-				{ (musicPlayer.videoId != null) && <img
-					style={{
-						maxWidth: 100,
-						height: "auto",
-						margin: "auto",
-						display: "block",
-						cursor: "pointer"
-					}}
-					src={musicPlayer.playing ? skipImage : ronImage}
-					onClick={() => dispatch({type: musicPlayer.playing ? ActionType.StopMusic : ActionType.PlayMusic})}
-				/>}
-			</Col>
-			<Col md="auto">
-				<i>
-					{secret
-						? "They said I could be anything, so I became yakitori."
-						: "We're going down, and sugar we're going down swinging."}
-					</i>
-				</Col>
-		</Row>
+	return <>
 		<Row className="mt-3">
 			<Teams session={currentSession} />
 		</Row>
 		<Row className="mt-3">
 			<LeagueStandingChart/>
 		</Row>
-		{ nextSession != null && <>
-			<Row className="px-4 py-3 justify-content-end" >
-				<Col md="auto" className="h4 mb-0"><u>Next Session</u></Col>
-			</Row>
-			<Row>
-				<Session session={nextSession}></Session>
-			</Row>
-		</>}
+			{ nextSession != null && <>
+				<Row className="px-4 py-3 justify-content-end" >
+					<Col md="auto" className="h4 mb-0"><u>Next Session</u></Col>
+				</Row>
+				<Row>
+					<Session session={nextSession}></Session>
+				</Row>
+			</>}
 		<Row className="px-4 py-3 justify-content-end" >
 			<Col md="auto" className="h4 mb-0"><u>Recent Session</u></Col>
 		</Row>
 		<Row>
 			<Session session={currentSession}></Session>
 		</Row>
-		<Row className="mt-3 justify-content-center">
-			<Col md="auto"><a className="text-dark" href="https://boards.4channel.org/vg/catalog#s=mjg">/mjg/</a></Col>
-			<Col md="auto"><a className="text-dark" href="https://repo.riichi.moe/">Repo</a></Col>
-			<Col md="auto"><a className="text-dark" href="https://github.com/riichinomics/majsoul-api">Source Code/Report Issue</a></Col>
-		</Row>
-	</Container>
+	</>
 }
