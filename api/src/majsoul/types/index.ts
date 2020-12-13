21ded45e09d2d0e82040d8d3eb7684b08f776cad
hierarch <riichinomics@gmail.com>
2020-12-13 18:38:08 +1300

    Added yakuman display

---
+++ api/src/majsoul/types/index.ts
@@ -0,0 +1,4 @@
+export { Player, GameResult, Contest, RoundInfo, AgariInfo, RoundResult } from "./types";
+export { DrawStatus } from "./DrawStatus";
+export { Wind } from "./Wind";
+export { Han } from "./Han";
