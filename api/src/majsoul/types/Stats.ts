c369ba9baf2016a6d2aa6e23a3c09d7ad7c6b9db
hierarch <riichinomics@gmail.com>
2021-05-08 01:52:29 +1200

    Linted stats code a bit

--- api/src/majsoul/types/Stats.ts
+++ api/src/majsoul/types/Stats.ts
@@ -1,4 +1,4 @@
-class RiichiStats {
+export interface RiichiStats {
 	total:       number;
 	daburiichi:  number;
 	furiten:     number;
@@ -10,22 +10,9 @@ class RiichiStats {
 	ippatsu:     number;
 	ura_hit: number;
 	immediate_dealin: number;
-	constructor() {
-		this.total       = 0;
-		this.daburiichi  = 0;
-		this.furiten     = 0;
-		this.sticks_kept = 0;
-		this.sticks_won  = 0;
-		this.first       = 0;
-		this.chase       = 0;
-		this.chased      = 0;
-		this.ippatsu     = 0;
-		this.ura_hit     = 0;
-		this.immediate_dealin = 0;
-	}
 }
 
-class DealerStats {
+export interface DealerStats {
 	rounds:           number;
 	tsumo_hit:        number; //someone else tsumos
 	tsumo_hit_points: number;
@@ -33,44 +20,23 @@ class DealerStats {
 	tsumo_hit_mangan_points: number;
 	repeats:           number;
 	max_repeats:       number;
-	constructor() {
-		this.rounds           = 0;
-		this.tsumo_hit        = 0;
-		this.tsumo_hit_points = 0;
-		this.tsumo_hit_mangan = 0;
-		this.tsumo_hit_mangan_points = 0;
-		this.repeats          = 0;
-		this.max_repeats      = 0;
-	}
 }
 
-class CallStats {
+export interface CallStats {
 	opened_hands: number; //hands opened
 	total:        number; //chinponyas
 	opps:         number; //opportunities
 	opps_all:     number; //'' including subsequent shouminkan/ankan chances
-	constructor() {
-		this.opened_hands = 0;
-		this.total        = 0;
-		this.opps         = 0;
-		this.opps_all     = 0;
-	}
 }
 
-class AgariStatsBreakdown {
+export interface AgariStatsBreakdown {
 	points:    number;
 	total:     number;
 	ron?:      number;
 	tsumo?:    number;
-	constructor() {
-		this.points    = 0;
-		this.total     = 0;
-		this.ron       = 0;
-		this.tsumo     = 0;
-	}
 }
 
-class AgariStats {
+export interface AgariStats {
 	yakuman: number; //double yakuman counts as 1
 	total:   number;
 	points:  number;
@@ -80,51 +46,26 @@ class AgariStats {
 	uradora: number;
 	akadora: number;
 	dora:    number;
-	constructor() {
-		this.total    = 0;
-		this.points   = 0;
-		this.yakuman  = 0;
-		this.open     = new AgariStatsBreakdown; //state of the winning hand; not the player's; in the scope of deal-ins
-		this.riichi   = new AgariStatsBreakdown;
-		this.dama     = new AgariStatsBreakdown;
-		this.uradora  = 0;
-		this.akadora  = 0;
-		this.dora     = 0;
-	}
 }
 
-class DrawStats {
+export interface DrawStats {
 	total:   number; //number of exhaustive draws in the game
 	points:  number;
 	ten:     number; //tenpai at draw
 	open:    number; //open hand at draw
 	riichi:  number;
 	nagashi: number;
-	constructor() {
-		this.total = 0; //number of exhaustive draws in the game
-		this.points = 0;
-		this.ten = 0; //tenpai at draw
-		this.open = 0; //open hand at draw
-		this.riichi = 0;
-		this.nagashi = 0;
-	}
 }
 
-class TurnStats {
+export interface TurnStats {
 	total:  number; //turns in the game; as counted by mjs
 	riichi: number; //sum of turn counts in riichi rounds
 	win:    number;
 	dealin: number;
-	constructor() {
-		this.total = 0; //turns in the game; as counted by mjs
-		this.riichi = 0; //sum of turn counts in riichi rounds
-		this.win = 0;
-		this.dealin = 0;
-	}
 }
 
-export class PlayerGameStats {
-	nhands:  number;          //total hands played in game
+export interface PlayerGameStats {
+	nhands:  number;
 	shanten: number;
 	riichi:  RiichiStats;
 	dealer:  DealerStats;
@@ -135,17 +76,90 @@ export class PlayerGameStats {
 	dealins_while_riichi: AgariStats;
 	draws:   DrawStats;
 	turns:   TurnStats;
-	constructor() {
-		this.nhands  = 0;
-		this.shanten = 0;
-		this.riichi  = new RiichiStats;
-		this.dealer  = new DealerStats;
-		this.calls   = new CallStats;
-		this.wins    = new AgariStats;
-		this.dealins = new AgariStats;
-		this.dealins_while_open   = new AgariStats;
-		this.dealins_while_riichi = new AgariStats;
-		this.draws = new DrawStats;
-		this.turns = new TurnStats;
+}
+
+export function createAgariStats(): AgariStats {
+	return {
+		yakuman: 0,
+		total: 0,
+		points: 0,
+		open: {
+			points: 0,
+			total: 0,
+			ron: 0,
+			tsumo: 0,
+		},
+		riichi: {
+			points: 0,
+			total: 0,
+			ron: 0,
+			tsumo: 0,
+		},
+		dama: {
+			points: 0,
+			total: 0,
+			ron: 0,
+			tsumo: 0,
+		},
+		uradora: 0,
+		akadora: 0,
+		dora: 0,
+	}
+}
+
+export function createStats(): PlayerGameStats {
+	return {
+		nhands: 0,
+		shanten: 0,
+
+		riichi: {
+			total: 0,
+			daburiichi: 0,
+			furiten: 0,
+			sticks_kept: 0,
+			sticks_won: 0,
+			first: 0,
+			chase: 0,
+			chased: 0,
+			ippatsu: 0,
+			ura_hit: 0,
+			immediate_dealin: 0,
+		},
+
+		dealer: {
+			rounds: 0,
+			tsumo_hit: 0,
+			tsumo_hit_points: 0,
+			tsumo_hit_mangan: 0,
+			tsumo_hit_mangan_points: 0,
+			repeats: 0,
+			max_repeats: 0,
+		},
+
+		calls: {
+			opened_hands: 0,
+			total: 0,
+			opps: 0,
+			opps_all: 0,
+		},
+
+		wins: createAgariStats(),
+		dealins: createAgariStats(),
+		dealins_while_open: createAgariStats(),
+		dealins_while_riichi: createAgariStats(),
+		draws: {
+			total: 0,
+			points: 0,
+			ten: 0,
+			open: 0,
+			riichi: 0,
+			nagashi: 0,
+		},
+		turns: {
+			total: 0,
+			riichi: 0,
+			win: 0,
+			dealin: 0,
+		},
 	}
 }
